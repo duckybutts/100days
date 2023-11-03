a37c5548f1208a80fdb088285b88c939ac7a3800
duckybutts <duckyrshannon@gmail.com>
2023-11-02 17:03:57 -0700

    Add project

---
+++ bandNameGenerator.py
@@ -0,0 +1,8 @@
+print("-----------------------------------------")
+print("Hello! Welcome to the band name generator")
+print("-----------------------------------------")
+city = input("What city did you grow up in? ")
+pet = input("What is the name of your pet? ")
+
+print("Your band name should be: " + city + pet + "!")
+print("-----------------------------------------")
