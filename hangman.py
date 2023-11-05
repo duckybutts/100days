c0062ed5d7150be695cb1106dce296d36b27d55f
duckybutts <duckyrshannon@gmail.com>
2023-11-04 17:54:57 -0700

    day7

---
+++ hangman.py
@@ -0,0 +1,325 @@
+import random
+
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /||  |
+ / |  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /||  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /||  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
+
+wordList = [
+'abruptly',
+'absurd',
+'abyss',
+'affix',
+'askew',
+'avenue',
+'awkward',
+'axiom',
+'azure',
+'bagpipes',
+'bandwagon',
+'banjo',
+'bayou',
+'beekeeper',
+'bikini',
+'blitz',
+'blizzard',
+'boggle',
+'bookworm',
+'boxcar',
+'boxful',
+'buckaroo',
+'buffalo',
+'buffoon',
+'buxom',
+'buzzard',
+'buzzing',
+'buzzwords',
+'caliph',
+'cobweb',
+'cockiness',
+'croquet',
+'crypt',
+'curacao',
+'cycle',
+'daiquiri',
+'dirndl',
+'disavow',
+'dizzying',
+'duplex',
+'dwarves',
+'embezzle',
+'equip',
+'espionage',
+'euouae',
+'exodus',
+'faking',
+'fishhook',
+'fixable',
+'fjord',
+'flapjack',
+'flopping',
+'fluffiness',
+'flyby',
+'foxglove',
+'frazzled',
+'frizzled',
+'fuchsia',
+'funny',
+'gabby',
+'galaxy',
+'galvanize',
+'gazebo',
+'giaour',
+'gizmo',
+'glowworm',
+'glyph',
+'gnarly',
+'gnostic',
+'gossip',
+'grogginess',
+'haiku',
+'haphazard',
+'hyphen',
+'iatrogenic',
+'icebox',
+'injury',
+'ivory',
+'ivy',
+'jackpot',
+'jaundice',
+'jawbreaker',
+'jaywalk',
+'jazziest',
+'jazzy',
+'jelly',
+'jigsaw',
+'jinx',
+'jiujitsu',
+'jockey',
+'jogging',
+'joking',
+'jovial',
+'joyful',
+'juicy',
+'jukebox',
+'jumbo',
+'kayak',
+'kazoo',
+'keyhole',
+'khaki',
+'kilobyte',
+'kiosk',
+'kitsch',
+'kiwifruit',
+'klutz',
+'knapsack',
+'larynx',
+'lengths',
+'lucky',
+'luxury',
+'lymph',
+'marquis',
+'matrix',
+'megahertz',
+'microwave',
+'mnemonic',
+'mystify',
+'naphtha',
+'nightclub',
+'nowadays',
+'numbskull',
+'nymph',
+'onyx',
+'ovary',
+'oxidize',
+'oxygen',
+'pajama',
+'peekaboo',
+'phlegm',
+'pixel',
+'pizazz',
+'pneumonia',
+'polka',
+'pshaw',
+'psyche',
+'puppy',
+'puzzling',
+'quartz',
+'queue',
+'quips',
+'quixotic',
+'quiz',
+'quizzes',
+'quorum',
+'razzmatazz',
+'rhubarb',
+'rhythm',
+'rickshaw',
+'schnapps',
+'scratch',
+'shiv',
+'snazzy',
+'sphinx',
+'spritz',
+'squawk',
+'staff',
+'strength',
+'strengths',
+'stretch',
+'stronghold',
+'stymied',
+'subway',
+'swivel',
+'syndrome',
+'thriftless',
+'thumbscrew',
+'topaz',
+'transcript',
+'transgress',
+'transplant',
+'triphthong',
+'twelfth',
+'twelfths',
+'unknown',
+'unworthy',
+'unzip',
+'uptown',
+'vaporize',
+'vixen',
+'vodka',
+'voodoo',
+'vortex',
+'voyeurism',
+'walkway',
+'waltz',
+'wave',
+'wavy',
+'waxy',
+'wellspring',
+'wheezy',
+'whiskey',
+'whizzing',
+'whomever',
+'wimpy',
+'witchcraft',
+'wizard',
+'woozy',
+'wristwatch',
+'wyvern',
+'xylophone',
+'yachtsman',
+'yippee',
+'yoked',
+'youthful',
+'yummy',
+'zephyr',
+'zigzag',
+'zigzagging',
+'zilch',
+'zipper',
+'zodiac',
+'zombie',
+]
+logo = ''' 
+ _                                             
+| |                                            
+| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+| '_  || _` | '_  | / _` | '_ ` _ | / _` | '_ |
+| | | | (_| | | | | (_| | | | | | | (_| | | | |
+|_| |_||__,_|_| |_||__, |_| |_| |_||__,_|_| |_|
+                    __/ |                      
+                   |___/    '''
+
+# choose a random word from the list
+word = random.choice(wordList)
+wordLen = len(word)
+charsRemaining = wordLen
+lives = 6  # initialize lives
+eog: bool = False
+
+# Create array of guesses
+guessList = []
+for letter in word:
+    guessList.append("_")
+
+print(logo)
+# Game Loop
+while eog == False:
+    # ask user to guess a letter
+    guess = input("Guess a letter: ").lower()
+
+    if guess in guessList:
+        print(f"You have already chosen {guess}")
+
+    # Check if guess matched anywhere in the letter, and move into array at correct location
+    for i in range(wordLen):
+        letter = word[i]
+        if letter == guess:
+            guessList[i] = letter
+
+    if "_" not in guessList:
+        print("You Win!")
+        eog == True
+
+    if guess not in word:
+        lives -= 1
+        print(f"{guess} is not in the word")
+        print(stages[lives])
+        if lives == 0:
+            print("You Lose.")
+            eog == True
+
+    print(f"{''.join(guessList)}")
+
